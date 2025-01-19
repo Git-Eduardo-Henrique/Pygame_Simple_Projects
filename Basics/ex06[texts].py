9fb769b79ee83871126216ac7d2f7000f23b83c7
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-19 00:07:37 -0300

    ex06

--- Basics/ex06[texts].py
+++ Basics/ex06[texts].py
@@ -7,14 +7,18 @@ pyg.init()
 
 resolution = 720, 480
 display = pyg.display.set_mode(resolution)
-pyg.display.set_caption("colições")
+pyg.display.set_caption("texto de pontos")
+
+# fonte, tamanho, negrito, italico
+font = pyg.font.SysFont("Arial", 40, True, True)
 
 x = resolution[0] / 2 - 25
 y = resolution[1] / 2 - 35
 
-# pos azul
 x_blue = randint(50, 670)
 y_blue = randint(50, 430)
+
+points = 0
 
 fps = pyg.time.Clock()
 
@@ -37,13 +41,19 @@ while True:
 
     display.fill((0, 0, 0))
 
-    # guardar em variaveis para modificar dps
+    pts = f"Points: {points}"
+    # variavel_da_fonte.render(texto, True, cor rgb)
+    rend_pts = font.render(pts, True, (255, 255, 255))
+
     red_rect = pyg.draw.rect(display, (255, 0, 0), (x, y, 50, 50))
     blue_rect = pyg.draw.rect(display, (0, 0, 255), (x_blue, y_blue, 50, 50))
 
-    # se colidir o azul vai para um local aleatorio
     if red_rect.colliderect(blue_rect):
         x_blue = randint(50, 670)
         y_blue = randint(50, 430)
 
+        points += 1
+
+    # para mostrar o texto na tela
+    display.blit(rend_pts, (500, 20)) # variavel com render, tupla com x e y
     pyg.display.update()
